e3baf4f64a106d85e443d2672011ee8a89362855
Nikolaos_Voulgaris <83508203+NickVoulg02@users.noreply.github.com>
2023-10-19 18:06:57 +0300

    Update inverted_index.py

--- inverted_index.py
+++ inverted_index.py
@@ -1,3 +1,5 @@
+import os
+
 class InvertedIndex:
 
     def __init__(self, doc):
@@ -13,25 +15,35 @@ class InvertedIndex:
         return words
             
     
-    def Inverted_Index(self):
+    def Inverted_Index(self, id):
         Inv_index = {}
         tokenlist = self.Tokenization()
         for x in range(len(tokenlist)):
             if Inv_index.get(tokenlist[x]) == None:
-                Inv_index[tokenlist[x]] = 1
-                
+                Inv_index[tokenlist[x]] = [1, [[id,1], [x+1]]]
 
             else:
-                Inv_index[tokenlist[x]]+=1
+                temp1 = Inv_index.get(tokenlist[x])
+                temp1[1][0][1]+=1
+                temp1[1][1].append(x+1)
 
         return Inv_index
 
 
 
 if __name__ == "__main__":
-    file = open("00001", 'r').read().splitlines()
-    inv = InvertedIndex(file)
-    #print(inv.Tokenization())
-    print(inv.Inverted_Index())
+            filename = "00001"
+    #for filename in os.listdir("C:/Users/nickv/Documents/information_retrieval/docs"):
+       #with open(os.path.join("C:/Users/nickv/Documents/information_retrieval/docs", filename), 'r') as f: # open in readonly mode
+            file = open(filename, 'r').read().splitlines()
+            id = int(filename)
+            inv = InvertedIndex(file)
+            #print(inv.Tokenization())
+            inv_index = inv.Inverted_Index(id)
+            print(inv_index.get("PSEUDOMONAS"))
+    
+    #Inv_index[0] = number of articles it appears in
+    #Inv_index[1][0] = id of article, number of appearances in specific article
+    #Inv_index 
     
     
